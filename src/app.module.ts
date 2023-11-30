4c6f867173a3003409d35558a67ae9fddeae47bf
Maksymilian Gala <94986692+maxidragon@users.noreply.github.com>
2023-11-30 13:24:34 +0100

    Code cleanup (#65)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [AuthModule, DbModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Param, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
-@UseGuards(AuthGuard('jwt'))
+
 @Controller('/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
