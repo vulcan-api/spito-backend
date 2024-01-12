a97ca42e21453bf59960414a54ce40112d4e990a
maxidragon <maksymilian.gala1@gmail.com>
2024-01-12 17:33:10 +0100

    Add routes to list and delete tokens

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { TokenService } from './token.service';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from '../auth/decorator/getUser.decorator';
@@ -13,5 +21,15 @@ export class TokenController {
   @Post()
   async createToken(@Body() dto: CreateTokenDto, @GetUser() user: JwtAuthDto) {
     return await this.tokenService.createToken(user.userId, dto);
+  }
+
+  @Get()
+  async getUsersTokens(@GetUser() user: JwtAuthDto) {
+    return await this.tokenService.getUsersTokens(user.userId);
+  }
+
+  @Delete(':id')
+  async deleteToken(@GetUser() user: JwtAuthDto, @Param() id: number) {
+    return await this.tokenService.deleteToken(+id, user.userId);
   }
 }
