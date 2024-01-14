ab8f480a31a3d12b0389b05e0091cd54d4d9b28c
maxidragon <maksymilian.gala1@gmail.com>
2024-01-14 19:27:30 +0100

    Add route token/verify

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -15,21 +15,28 @@ import { GetUser } from '../auth/decorator/getUser.decorator';
 import { JwtAuthDto } from '../auth/dto';
 import { CreateTokenDto } from './dto/createToken.dto';
 
-@UseGuards(AuthGuard('jwt'))
 @Controller('token')
 export class TokenController {
   constructor(private readonly tokenService: TokenService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   async createToken(@Body() dto: CreateTokenDto, @GetUser() user: JwtAuthDto) {
     return await this.tokenService.createToken(user.userId, dto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async getUsersTokens(@GetUser() user: JwtAuthDto) {
     return await this.tokenService.getUsersTokens(user.userId);
   }
 
+  @Get('verify/:token')
+  async verifyToken(@Param('token') token: string) {
+    return await this.tokenService.verifyToken(token);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   async deleteToken(@GetUser() user: JwtAuthDto, @Param('id') id: number) {
