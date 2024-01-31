65e90794bb08baf8c7e7f82634bbd36a3d2c38e6
maxidragon <maksymilian.gala1@gmail.com>
2024-01-31 17:02:41 +0100

    Profile overview

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Param, Get } from '@nestjs/common';
+import { Controller, Param, Get, Query, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { GetUser } from 'src/auth/decorator/getUser.decorator';
+import { JwtAuthDto } from 'src/auth/dto';
+import { OptionalJwtAuthGuard } from 'src/auth/guards/optionalJwtAuth.guard';
 
-@Controller('/user')
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/:userId')
+  @Get(':userId')
   async getPublicInformation(@Param('userId') userId: number) {
-    return this.userService.getPublicInformation(userId);
+    return await this.userService.getPublicInformation(userId);
+  }
+
+  @UseGuards(OptionalJwtAuthGuard)
+  @Get('activity/:userId')
+  async getUserActivity(
+    @Param('userId') userId: number,
+    @Query('from')
+    from = new Date(new Date().setMonth(new Date().getMonth() - 1)),
+    @Query('to') to = new Date(),
+    @GetUser() user: JwtAuthDto,
+  ) {
+    return await this.userService.getUserActivity(
+      userId,
+      from,
+      to,
+      user.userId,
+    );
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,4 +17,53 @@ export class UserService {
     );
     return userPublicInformation;
   }
+
+  async getUserActivity(
+    userId: number,
+    from: Date,
+    to: Date,
+    requestedBy: number,
+  ) {
+    const data = await this.prisma.user.findFirst({
+      where: { id: userId },
+      select: {
+        id: true,
+        rulesets: {
+          select: {
+            id: true,
+            name: true,
+          },
+          where: {
+            createdAt: {
+              gte: new Date(from),
+              lte: new Date(to),
+            },
+          },
+        },
+        Environment: {
+          select: {
+            id: true,
+            isPrivate: true,
+            name: true,
+          },
+          where: {
+            createdAt: {
+              gte: new Date(from),
+              lte: new Date(to),
+            },
+          },
+        },
+      },
+    });
+    return {
+      createdRulesets: data.rulesets,
+      createdEnvironments: data.Environment.map((env) => {
+        if (env.isPrivate) {
+          return data.id === requestedBy && env;
+        } else {
+          return env;
+        }
+      }),
+    };
+  }
 }
